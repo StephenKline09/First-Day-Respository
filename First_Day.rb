465fcea998f40253752ecadccae75518a4f0251a
Stephen Kline <Stephenkline09@gmail.com>
2020-07-06 19:06:20 -0500

    Car Inventory

--- First_Day.rb
+++ First_Day.rb
@@ -1,2 +1,9 @@
-puts "Hellooooo, My Dude"
-puts "Are you confused yet?"
+puts "Car Inventory"
+puts "What is the make of the car?"
+make = gets
+puts "What is the model of the car?"
+model = gets
+puts "What is the color of the car?"
+color = gets
+puts "What is the year of the car?"
+year = gets
