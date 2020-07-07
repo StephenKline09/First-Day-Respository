6b84e12468cf7c30fdb052db13cb746305cdfd3b
Stephen Kline <Stephenkline09@gmail.com>
2020-07-06 19:30:29 -0500

    Borderlands & Game of Thrones

---
+++ Borderlands_Weapons.rb
@@ -0,0 +1,9 @@
+puts "Best Borderlands 3 Weapons 2020"
+puts "Best Shotgun"
+shotgun = gets
+puts "Best Sniper Rifle"
+sniper rifle = gets
+puts "Best Assault Rifle"
+assault rifle = gets
+puts "Best Pistol"
+pistol = gets
---
+++ Top_GoT_Characters.rb
@@ -0,0 +1,9 @@
+puts "Greatest Characters in Game of Thrones"
+puts "Most Noble Character"
+noble = gets
+puts "Deadliest Character"
+deadliest = gets
+puts "Scariest Character"
+scariest = gets 
+puts "Most Mysterious Character"
+mysterious = gets 
