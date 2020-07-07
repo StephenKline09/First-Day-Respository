4bcc973d39a7bd438dc76263393fd3c74f8880c3
Stephen Kline <Stephenkline09@gmail.com>
2020-07-06 19:12:31 -0500

    Marvel Test File

---
+++ List_of_Marvel_Characters.rb
@@ -0,0 +1,9 @@
+puts "Marvel Characters"
+puts "Who is your first character?"
+first = gets
+puts "Who is your second character?"
+second = gets 
+puts "Who is your third character?"
+third = gets
+puts "Who is your favorite character?"
+favorite = gets
