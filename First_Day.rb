37f8a177b3dc983a42840e7206a7b57d9cf78662
Stephen Kline <Stephenkline09@gmail.com>
2020-07-06 19:41:52 -0500

    Changes maybe?

--- First_Day.rb
+++ First_Day.rb
@@ -1,9 +1,9 @@
 puts "Car Inventory"
 puts "What is the make of the car?"
-make = gets
+make = gets.chomp
 puts "What is the model of the car?"
-model = gets
+model = gets.chomp
 puts "What is the color of the car?"
-color = gets
+color = gets.chomp
 puts "What is the year of the car?"
-year = gets
+year = gets.chomp
