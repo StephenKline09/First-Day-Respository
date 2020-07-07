889733f615cd2ab13205ef5ce7484189f3e63eb6
Stephen Kline <Stephenkline09@gmail.com>
2020-07-06 19:37:37 -0500

    Bl3 weapons

--- Borderlands_Weapons.rb
+++ Borderlands_Weapons.rb
@@ -2,8 +2,8 @@ puts "Best Borderlands 3 Weapons 2020"
 puts "Best Shotgun"
 shotgun = gets
 puts "Best Sniper Rifle"
-sniper rifle = gets
+sniper = gets
 puts "Best Assault Rifle"
-assault rifle = gets
+assault = gets
 puts "Best Pistol"
 pistol = gets
