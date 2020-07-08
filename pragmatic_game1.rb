1ca9987c244a787cb7adabc2fe5429aae17b779e
Stephen Kline <Stephenkline09@gmail.com>
2020-07-08 11:46:49 -0500

    Pragmatic Studios Day 2 Completed

--- pragmatic_game1.rb
+++ pragmatic_game1.rb
@@ -1,19 +1,14 @@
-player1 = "stephen"
-hp1 = 30
+def say_hello(name, health=100)
+    "I'm #{name.capitalize} with a health of #{health} as of #{time}"
+end
 
-player2 = "emily"
-hp2 = 60
+def time
+    current_time = Time.new
+    current_time.strftime("%I:%M:%S")
+end
 
-player3 = "pierce_hawthorn"
-hp3 = 100
-
-player4 = "floof and bobeira"
-hp4 = 25
-
-puts "#{player1.capitalize}'s health is #{hp1}"
-puts "#{player2.upcase}'s health is #{hp2}"
-puts "#{player3.capitalize}'s health is #{hp3*2}".center(50, '*')
-puts "#{player4.capitalize.ljust(30, '.')} #{hp4} hp"
-
-puts "Players: \n\t#{player1}\n\t#{player2}\n\t#{player3}"
+puts say_hello("stephen", 125)
+puts say_hello("emily", 120)
+puts say_hello("floof")
+puts say_hello("bobeira")
 
