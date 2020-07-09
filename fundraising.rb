0b3bd9ce73d94cad9a4b01c1d37fcdbc3d32ce93
Stephen Kline <Stephenkline09@gmail.com>
2020-07-09 17:21:43 -0500

    Attempting to Save

---
+++ fundraising.rb
@@ -0,0 +1,18 @@
+puts "Project ABC has $1000 in funding."
+
+def projects
+    project = #{name, funds raised}
+end
+
+def time
+    current_time = Time.new
+    formatted_time = current_time.strftime("%A %m/%d%Y at %I:%M%p")
+end
+
+puts "Projects began on #{formatted_time}"
+ 
+puts "Projects:"
+puts project(ABC)
+puts project(GHI)
+puts project(XYZ)
+ 
