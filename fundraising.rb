137543a2eca5d46261bb31665db175517d958fa7
Stephen Kline <Stephenkline09@gmail.com>
2020-07-13 08:39:56 -0500

    commits

--- fundraising.rb
+++ fundraising.rb
@@ -1,18 +1,25 @@
-def projects
-    project = #{name, funds raised}
+class Project
+    def initialize(name, initial=1000, target)
+        @name = name.capitalize
+        @initial = initial_funds
+        @target = target_goal
+    end
+
+    def to_s
+        "#{name} currently has #{funds}"
+    end
+
+    def add
+        @initial += 100
+        puts "#{@name} gained $100 in funding!"
+    end 
+
+    def loss
+        @initial -= 100
+        puts "#{@name} lost $100 in funding.."
+    end
 end
 
-def time
-    current_time = Time.new
-    formatted_time = current_time.strftime("%A %m/%d%Y at %I:%M%p")
-end
-
-
-puts "Projects began on #{formatted_time}"
- 
-puts "Projects:"
-puts project(ABC)
-puts project(GHI)
-puts project(XYZ)
- 
-  
+project1 = Project.new("ABC")
+puts project1
+    
--- pragmatic_game1.rb
+++ pragmatic_game1.rb
@@ -1,14 +1,32 @@
-def say_hello(name, health=100)
-    "I'm #{name.capitalize} with a health of #{health} as of #{time}"
+class Player
+    def initialize(name, health=100)
+        @name = name.capitalize
+        @health = health
+    end
+
+    def to_s
+        "I'm #{@name} with a health of #{@health}."
+    end
+
+    def blam
+        @health -= 10
+        puts "#{@name} got blammed!"
+    end
+
+    def w00t
+        @health += 15
+        puts "#{@name} got w00ted!"
+    end
 end
 
-def time
-    current_time = Time.new
-    current_time.strftime("%I:%M:%S")
-end
+player1 = Player.new("moe")
+puts player1
 
-puts say_hello("stephen", 125)
-puts say_hello("emily", 120)
-puts say_hello("floof")
-puts say_hello("bobeira")
- 
+player2 = Player.new("larry", 60)
+puts player2
+
+player3 = Player.new("curly", 125)
+puts player3
+
+player1.blam
+puts player1
